8a2ac67016424c9bec70a0da2cee39c6cffdbe4d
kieran poropat <poro0002@algonquinlive.com>
2024-08-02 12:08:42 -0400

    sorts data and and added commands to userinput, running app while loop

--- data.gov_filter.cpp
+++ data.gov_filter.cpp
@@ -31,6 +31,9 @@
 using namespace std;
 using json = nlohmann::json;
 
+// OPTIONS
+
+
 // Callback function to handle data (if you want to process response data)
 size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
 
@@ -50,33 +53,37 @@ size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
 }
 
 
-void searchJobByTitle(const json& jsonData, const string& jobTitle) {
 
-    cout << "json object: " << jsonData.dump(4) << endl;
-    
-    
+void logJobTitles(const json& jsonData) {
+
+
     if (jsonData.contains("data")) {
         const auto& data = jsonData["data"];
 
         if (data.is_array()) {
-            cout << "'data' is an array." << endl;
-        for (const auto& item : data) {
-            if (item.is_array() && item.size() > 8) {
-                string title = item[8].get<string>();  // for each peice of data it puts the "job title" into a title string variable
+         /*   cout << "'data' is an array." << endl; */
+            vector<string> vec;
+            
+            cout << "\n" << endl;
+            for (const auto& item : data) {
+                if (item.is_array() && item.size() > 8) {
+                    string title = item[8].get<string>();  // for each peice of data it puts the "job title" into a title string variable
 
-             // so this makes it so it will look to match the user string with the corrosponding match in the data
-                if (title.find(jobTitle) != string::npos) {
-                    // Print the details of the job if it matches
-                    cout << "Job Title: " << title << endl;
-                    cout << "Code: " << item[1].get<string>() << endl;
-                    cout << "Salary: " << item[11].get<string>() << endl;
-                    cout << "Grade: " << item[10].get<string>() << endl;
-                    cout << "Number: " << item[12].get<string>() << endl;
-                    cout << endl;
+                  
+                    vec.push_back(title);
+                 
+
                 }
             }
+
+            sort(vec.begin(), vec.end());
+
+                for (const auto& title : vec) {
+                    cout << title << endl;
+                };
+
+         
         }
-    }
         else {
             cout << "'data' is not an array" << endl;
         }
@@ -84,7 +91,49 @@ void searchJobByTitle(const json& jsonData, const string& jobTitle) {
     else {
         cout << "Does not contain 'data'" << endl;
     }
-}
+};
+
+
+
+void searchJobByTitle(const json& jsonData, const string& userInput) {
+
+    /*cout << "json object: " << jsonData.dump(4) << endl;*/  // this is to log raw data
+
+
+
+
+
+    if (jsonData.contains("data")) {
+        const auto& data = jsonData["data"];
+
+        if (data.is_array()) {
+           /* cout << "'data' is an array." << endl;*/
+            for (const auto& item : data) {
+                if (item.is_array() && item.size() > 8) {
+                    string title = item[8].get<string>();  // for each peice of data it puts the "job title" into a title string variable
+
+                    // so this makes it so it will look to match the user string with the corrosponding match in the data
+                    if (title.find(userInput) != string::npos) {
+                        // Print the details of the job if it matches
+                        cout << "Job Title: " << title << endl;
+                        cout << "Code: " << item[1].get<string>() << endl;
+                        cout << "Salary: " << item[11].get<string>() << endl;
+                        cout << "Grade: " << item[10].get<string>() << endl;
+                        cout << "Number: " << item[12].get<string>() << endl;
+                        cout << endl;
+                    }
+                }
+            }
+         
+        }
+        else {
+            cout << "'data' is not an array" << endl;
+        }
+    }
+    else {
+        cout << "Does not contain 'data'" << endl;
+    }
+};
 
 
 
@@ -94,7 +143,7 @@ int main() {
 
 
 
-    // Initialize CURL
+    // Initialize CURL and these curl keywords point to these variables that will hold coro data 
     CURL* curl;
     CURLcode res;
     string readBuffer;
@@ -107,8 +156,8 @@ int main() {
 
     }
 
-   // set the URL to fetch from
- // Set the URL to fetch JSON data
+    // set the URL to fetch from
+  // Set the URL to fetch JSON data
     curl_easy_setopt(curl, CURLOPT_URL, "https://data.montgomerycountymd.gov/api/views/48wg-fkab/rows.json?accessType=DOWNLOAD");
 
     struct curl_slist* headers = NULL; // no headers set
@@ -142,19 +191,42 @@ int main() {
     json jsonData;
     try {
         jsonData = json::parse(readBuffer);
-        cout << "Fetched data: " <<  jsonData.dump(4) << endl;
+        /* cout << "Fetched data: " <<  jsonData.dump(4) << endl;*/
     }
     catch (const json::parse_error& e) {
         cerr << "JSON parse error: " << e.what() << endl;
         return -1;
     }
 
-    // Search for a specific job title
-    string jobTitle;
-    cout << "Enter the job title to search: ";
-    getline(cin, jobTitle);
-    searchJobByTitle(jsonData, jobTitle); // you will the the user input value to use with the search function, obvi you will need the json aswell
+ 
 
-    
+    // Search for a specific job title
+    string userInput;
+
+
+    bool running = true;
+
+    while (running) {
+        cout << "\n" << endl;
+        cout << "Use The Command /list to see the job options or exit with /exit" << endl;
+        cout << "Enter the job title to search:";
+        getline(cin, userInput); // this will only run if you click enter. once that finishes it will run the nest line in your code 
+
+        if (userInput == "/list") {
+            cout << "\n" << endl;
+            logJobTitles(jsonData);
+        }
+        else if (userInput == "/exit") {
+            running = false; // the while loop will quit running the job titles function and c-input 
+        }
+        else {
+            cout << "\n" << endl;
+            searchJobByTitle(jsonData, userInput); // you will the the user input value to use with the search function, obvi you will need the json aswell
+
+        }
+    };
+   
+
+
     return 0;
-}
+};
